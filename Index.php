2a2d4c0a3d70bb7d8aef0ca152ba5b6ede719390
Ninefrm <maxfonseca.r@gmail.com>
2019-10-31 20:21:45 -0600

    Graphics

--- Index.php
+++ Index.php
@@ -17,9 +17,50 @@ $resultado = $statement->fetch();
 <body>
 
 <?php include 'Visual/Plantilla/Header.php'; ?>
-<section class="main">
-   <p>Hola.</p>
-</section>
+
+<table>
+    <thead>
+    <tr>
+        <th class="card blue-grey darken-1">TOP #</th>
+    </tr>
+    </thead>
+    <tbody>
+    <div class="container">
+        <td>
+            <div class="carousel">
+                <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
+                <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
+                <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
+                <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"></a>
+                <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"></a>
+            </div>
+        </td>
+
+    </div>
+    </tbody>
+</table>
+
+<table>
+    <thead>
+    <tr>
+        <th class="card blue-grey darken-1">TOP #</th>
+    </tr>
+    </thead>
+    <tbody>
+    <td>
+        <div class="container">
+            <div class="carousel">
+                <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
+                <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
+                <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
+                <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"></a>
+                <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"></a>
+            </div>
+        </div>
+    </td>
+    </tbody>
+</table>
+
 
 </body>
 <?php include 'Visual/Plantilla/PieDePagina.php'; ?>
